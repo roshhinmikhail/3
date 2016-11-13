03a1359be5199346d1c122ac5e22a6f90b872f10
Mikhail <roshinmikhail2016@yandex.ru>
2016-11-13 18:12:08 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a, b, c;
+            Console.WriteLine("Введите число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите степень");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a;
+            while (b > 1) { a = a * c; b--; }
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
